7c92725e1b39d38c61f247460e6e55722fd74058
hoogie <hugrun13@ru.is>
2015-03-30 15:30:07 +0000

    music

---
+++ app/scripts/audio.js
@@ -0,0 +1,20 @@
+var music = document.getElementById('music');
+ 
+function playAudio() {
+	
+	if (music.muted) {
+		music.muted = false;
+		music.play();
+		pButton.className = "play";
+	}
+	else { 
+		music.muted = true;
+		pButton.className = "mute";
+	}
+}
+
+/*function toggleSound(img)
+{
+   img.src= img.src=="http://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Speaker_Icon.svg/500px-Speaker_Icon.svg.png" ? "https://cdn2.iconfinder.com/data/icons/picons-essentials/57/music_off-512.png" : "http://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Speaker_Icon.svg/500px-Speaker_Icon.svg.png";
+}*/
+
