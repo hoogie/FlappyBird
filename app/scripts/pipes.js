8adb9f894c7f968086d4cb96aa91b571cd0c1b7b
Perla <perla11@ru.is>
2015-03-30 14:49:34 +0000

    -a

---
+++ app/scripts/pipes.js
@@ -0,0 +1,22 @@
+window.Pipe = (function() {
+	'use strict';
+
+	var pipeheight = 90;
+	var pipewidth = 52;
+	var pipe = new Array();
+
+})
+function updatePipes()
+{
+   //Do any pipes need removal?
+   $(".pipe").filter(function() { return $(this).position().left <= -100; }).remove()
+   
+   //add a new pipe (top height + bottom height  + pipeheight == 420) and put it in our tracker
+   var padding = 80;
+   var constraint = 420 - pipeheight - (padding * 2); //double padding (for top and bottom)
+   var topheight = Math.floor((Math.random()*constraint) + padding); //add lower padding
+   var bottomheight = (420 - pipeheight) - topheight;
+   var newpipe = $('<div class="pipes"><div class="upperPipe" style="height: ' + topheight + 'px;"></div><div class="lowerPipe" style="height: ' + bottomheight + 'px;"></div></div>');
+   $("#flyarea").append(newpipe);
+   pipes.push(newpipe);
+}
