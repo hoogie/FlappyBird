b01ea6f65fb13d9a2a66a5e0e32664173f72dc8d
asrunes <asrun13@ru.is>
2015-04-02 01:35:18 +0000

    krassar á efri og neðri volume1

--- app/scripts/pipes.js
+++ app/scripts/pipes.js
@@ -3,9 +3,10 @@ window.Pipe = (function() {
 
    //var Controls = window.Controls;
 
-
+   var POINTS = 0;
    var INITIAL_POSITION_X = 0;
    var INITIAL_POSITION_Y = 0;
+
 
    var WIDTH = 5;
 
@@ -27,16 +28,19 @@ window.Pipe = (function() {
                var tempY = randomNumber(-4, 4);
                this.elupper.pos = { x: 100, y: tempY };
                this.ellower.pos = { x: 100, y: tempY - randomNumber(9, 13) };
+               
       }
-      if(this.vers === 2) {
+      if(this.vers === 2) {         
                var tempY = randomNumber(-4, 4);
                this.elupper.pos = { x: 130, y: tempY };
                this.ellower.pos = { x: 130, y: tempY - randomNumber(9, 13) };
+               
       }
-      if(this.vers === 3){
+      if(this.vers === 3){           
                var tempY = randomNumber(-4, 4);
                this.elupper.pos = { x: 160, y: tempY };
                this.ellower.pos = { x: 160, y: tempY - randomNumber(9, 13) };
+               
 
       }
 
@@ -62,18 +66,30 @@ window.Pipe = (function() {
       //console.log(this.pos.y);
       //stopper = 0;
    };*/
+   var tempGap = 10;
 
    Pipe.prototype.reset = function() {
  
       if(this.elupper.pos.x < -WIDTH) {
-    
+         tempGap = randomNumber(9,13);
          this.elupper.pos.x = 100;
          this.elupper.pos.y = randomNumber(-4, 4);
          this.ellower.pos.x = 100;
-         this.ellower.pos.y = this.elupper.pos.y - randomNumber(9, 13);
+         this.ellower.pos.y = this.elupper.pos.y - tempGap;
+         console.log(tempGap);
       }
 
+
    };
+
+   /*function playerScore() {
+   //score += 1;
+   //console.log(score);
+   //play score sound
+   /*soundScore.stop();
+   soundScore.play();
+   setBigScore();*/
+
 
    Pipe.prototype.onFrame = function(delta) {
       
@@ -82,6 +98,7 @@ window.Pipe = (function() {
       this.ellower.pos.x -= delta * 20;
      
          this.reset();
+         //playerScore();
       
     
       this.checkCollisionWithPipe();
@@ -90,72 +107,68 @@ window.Pipe = (function() {
      
    };
 
+
+
    Pipe.prototype.checkCollisionWithPipe = function() {
-    /* if(this.vers === 1) {
-         console.log(this.ellower.pos.x);
-         console.log("this.pos.x");
-         console.log(this.game.player.pos.x);
-
-         console.log("this.game.player");
-      }*/
-      if(this.vers === 1) {
-
-             console.log(this.elupper.pos.x, "upper X");
-             console.log(this.game.player.pos.x, "player x");
-             console.log(this.elupper.pos.y, "upper y");
-             console.log(this.game.player.pos.y, "player y");
-      }
-             
-      
 
       //player height = 6, width = 8
       //pipuhattar  width: 5.2em; height: 2.6em;
       //pipur width 5.2 og height 20
-     if(this.vers === 1 && (this.elupper.pos.x > this.game.player.pos.x)) {
-         if((this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 > this.game.player.pos.y)/* ||
-            (this.ellower.pos.x > this.game.player.pos.x + 8) && (this.ellower.pos.y < this.game.player.pos.y)*/) {
+     if(this.vers === 1) {
+      if(this.elupper.pos.x > this.game.player.pos.x) {
+         console.log(tempGap, "tempGat í check");
+         if((this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 > this.game.player.pos.y) ||
+            (this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 + tempGap < this.game.player.pos.y)) {
                 console.log("----upperbumb----------");
              
 
                   this.game.player.playerDead();
                   return this.game.gameover()
          }
-        /* if((this.ellower.pos.x < this.game.player.pos.x + 8) && (this.ellower.pos.y < this.game.player.pos.y + 6))
-         {
-            console.log("----lowerbumb----------");
-            this.game.player.playerDead();
-            //return this.game.gameOver();
-         }*/
+
+         
+      
       }
-    /*  if(this.vers === 2) {
-         if((this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 > this.game.player.pos.y) ) {
-                console.log("--------------------------------------------------------------------");
+   }
+ 
+
+       if(this.vers === 2) {
+           if(this.elupper.pos.x > this.game.player.pos.x) {
+       
+         if((this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 > this.game.player.pos.y) ||
+            (this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 + tempGap < this.game.player.pos.y)) {
+                console.log("----upperbumb222222222222222222----------");
+             
 
                   this.game.player.playerDead();
-        //return this.game.gameover();
-        //this.game.player.gameOver();
+                  return this.game.gameover()
          }
+     
+ 
       }
-      if(this.vers === 2) {
-         if((this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 > this.game.player.pos.y)) {
-                console.log("--------------------------------------------------------------------");
+}
+          
+       if(this.vers === 3) {
+           if(this.elupper.pos.x > this.game.player.pos.x) {
+       
+         if((this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 > this.game.player.pos.y) ||
+            (this.elupper.pos.x < this.game.player.pos.x + 8) && (this.elupper.pos.y + 20 + tempGap < this.game.player.pos.y)) {
+                console.log("----upperbumb33333333333333333333----------");
+             
 
                   this.game.player.playerDead();
-        //return this.game.gameover();
-        //this.game.player.gameOver();
+                  return this.game.gameover()
          }
-      }*/
+     
+ 
+      }
+}
+
+         
 
       };
 
 
-
-     /* if (this.pos.x < 0 ||
-         this.pos.x + WIDTH > this.game.WORLD_WIDTH ||
-         this.pos.y < 0 ||
-         this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
-         return this.game.gameover();
-      } */
    
 
    return Pipe;
